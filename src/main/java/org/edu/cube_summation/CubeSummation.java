d03700ba26163785b1fa7fdac342d4fba5081ac5
diegomurbanos <diego@mensajerosurbanos.com>
2016-06-21 19:40:38 -0500

    perform update operation

--- src/main/java/org/edu/cube_summation/CubeSummation.java
+++ src/main/java/org/edu/cube_summation/CubeSummation.java
@@ -1,5 +1,7 @@
 package org.edu.cube_summation;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -14,12 +16,13 @@ public class CubeSummation {
 
         T = in.nextInt();
         while (T > 0) {
+            Map<Integer, Long> map = new HashMap<>();
             N = in.nextInt();
             M = in.nextInt();
             while (M > 0) {
                 String str = in.next();
                 if (str.equals("UPDATE")) {
-
+                    update(map, in.nextInt(), in.nextInt(), in.nextInt(), in.nextLong(), N);
                 } else {
 
                 }
@@ -27,6 +30,10 @@ public class CubeSummation {
             T--;
         }
 
+    }
+
+    public static void update(Map<Integer, Long> map, int x, int y, int z, long value, int N) {
+        map.put(x + (y * N) + (z * N * N), value);
     }
 
 }
