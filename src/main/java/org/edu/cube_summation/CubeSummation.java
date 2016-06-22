dcd22a0ca39d9291804d61f1d269c88e661d8d72
diegomurbanos <diego@mensajerosurbanos.com>
2016-06-21 19:34:35 -0500

    perform stdin

--- src/main/java/org/edu/cube_summation/CubeSummation.java
+++ src/main/java/org/edu/cube_summation/CubeSummation.java
@@ -1,9 +1,32 @@
 package org.edu.cube_summation;
 
+import java.util.Scanner;
+
 /**
  *
  * @author diego
  */
 public class CubeSummation {
-     
+
+    public static void main(String[] args) {
+        int T, N, M;
+        Scanner in = new Scanner(System.in);
+
+        T = in.nextInt();
+        while (T > 0) {
+            N = in.nextInt();
+            M = in.nextInt();
+            while (M > 0) {
+                String str = in.next();
+                if (str.equals("UPDATE")) {
+
+                } else {
+
+                }
+            }
+            T--;
+        }
+
+    }
+
 }
