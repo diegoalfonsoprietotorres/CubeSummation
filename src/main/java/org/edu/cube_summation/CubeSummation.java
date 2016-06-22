6f427eb9ff4843f33a35bfabc5064dc4ea44fde3
diegomurbanos <diego@mensajerosurbanos.com>
2016-06-21 20:00:33 -0500

    perform query operation

--- src/main/java/org/edu/cube_summation/CubeSummation.java
+++ src/main/java/org/edu/cube_summation/CubeSummation.java
@@ -24,8 +24,9 @@ public class CubeSummation {
                 if (str.equals("UPDATE")) {
                     update(map, in.nextInt(), in.nextInt(), in.nextInt(), in.nextLong(), N);
                 } else {
-
+                    query(map, N, in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt());
                 }
+                M--;
             }
             T--;
         }
@@ -37,6 +38,20 @@ public class CubeSummation {
         y--;
         z--;
         map.put(x + (y * N) + (z * N * N), value);
+    }
+
+    public static void query(Map<Integer, Long> map, int N, int x1, int y1, int z1, int x2, int y2, int z2) {
+        long sum = 0;
+        for (Map.Entry<Integer, Long> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int x = (key % N) + 1;
+            int y = ((key / N) % N) + 1;
+            int z = (key / (N * N)) + 1;
+            if (x1 <= x && x <= x2 && y1 <= y && y <= y2 && z1 <= z && z <= z2) {
+                sum += entry.getValue();
+            }
+        }
+        System.out.println(sum);
     }
 
 }
